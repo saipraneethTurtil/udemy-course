72c970d11732227844332d9045a97de3c3e0df53
saipraneethTurtil <saipraneeth.talluri@turtil.co>
2023-12-09 16:18:25 +0530

    Conditional Re-Render

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import "./App.css";
 import PersInfo from "./components/PersInfo";
 import { useState, useEffect } from "react";
 
-// Creating a Digital Clock
+// Conditional Rerendering
+
+let gCnt = 0;
 function App() {
-  const [time, setTime] = useState(new Date().toLocaleString());
-  useEffect(() => {
-    setInterval(() => {
-     
-      setTime(new Date().toLocaleString());
-      console.log(time)
-    }, 1000);
+  const [Obj, setObj] = useState({
+    cnt: 10,
+    flag: true,
   });
+  useEffect(() => {
+    console.log("Exection called ", gCnt++);
+  }, [Obj.cnt]);
 
   return (
     <div className="App">
-      <h1>Time is : {time}</h1>
+      <h1>
+        {Obj.cnt}-{Obj.flag.toString()}
+      </h1>
+      <button
+        onClick={() => {
+          setObj({ ...Obj, cnt: Obj.cnt + 1 });
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setObj({ ...Obj, cnt: Obj.cnt - 1 });
+        }}
+      >
+        Decrement
+      </button>
+      <button
+        onClick={() => {
+          setObj({ ...Obj, flag: !Obj.flag });
+        }}
+      >
+        Toggle
+      </button>
     </div>
   );
 }
